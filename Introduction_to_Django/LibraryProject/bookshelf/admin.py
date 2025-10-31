7ceb1d45182aeafc2abc7e1e452f55a5f1f8cf5c
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-10-31 06:19:52 -0700

    Register Book model in admin with list_display, list_filter, search_fields

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,5 +3,14 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date')
-    search_fields = ('title', 'author')
+    # Columns to show in the change list (table) view
+    list_display = ("id", "title", "author", "publication_year")
+    # Add filters in the right sidebar
+    list_filter = ("author", "publication_year")
+    # Add a search box (searches these fields)
+    search_fields = ("title", "author")
+    # Optional: default ordering
+    ordering = ("-publication_year", "title")
+    # Optional: number of items per page in the list view
+    list_per_page = 25
+
