f37c911c6580b332a2a694876324dd93c3c68989
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-11-06 07:09:57 -0800

    Add relationship_app templates: list_books and library_detail

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView  # ✅ Required imports
+from .views import list_books, LibraryDetailView
+
+app_name = "relationship_app"
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # Function-based view
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
 
 
