4450c420bbb5bd1a1c7431b9cdc0b692c79deb64
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-11-06 07:36:30 -0800

    Add relationship_app templates: list_books and library_detail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Library, Book   # ✅ This is the missing line you need!
 
+# Function-Based View
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+# Class-Based View
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
