e1b032069a6cb66c8693540b57b09182f0fca335
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-11-07 03:56:59 -0800

    Add register, login, and logout routes to relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import list_books, LibraryDetailView, register
+from . import views
+
 
 app_name = "relationship_app"
 
@@ -13,4 +15,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
