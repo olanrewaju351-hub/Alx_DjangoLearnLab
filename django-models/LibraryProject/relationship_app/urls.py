64f8c53815ee7560ff302286f4b1ba8cfbd7f397
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-11-07 06:40:33 -0800

    Add custom Book permissions and permission-protected add/edit/delete views and URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
+
     
     # auth
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
