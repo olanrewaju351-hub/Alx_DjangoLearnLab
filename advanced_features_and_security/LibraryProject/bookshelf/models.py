9e9a5f655301f601b71dab6ee7d04dc2dd32f1b6
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-11-13 03:04:16 -0800

    feat: add CustomUser model with date_of_birth and profile_photo, update settings and relationships

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
