c9d8d1c1afc26d1f8e286168cd5dee3c1ec8a471
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-11-13 02:47:39 -0800

    feat: add CustomUser model with date_of_birth and profile_photo, update settings and relationships

--- advanced_features_and_security/LibraryProject/accounts/models.py
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -14,6 +14,13 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
         return self.create_user(username, email, password, **extra_fields)
 
 
