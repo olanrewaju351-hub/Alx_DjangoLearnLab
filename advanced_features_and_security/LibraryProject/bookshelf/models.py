980efebad8a3266e03f7babc62706f2d9d034bfc
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-11-13 02:36:15 -0800

    feat: add CustomUser model with date_of_birth and profile_photo, update settings and relationships

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField(null=True, blank=True)
+
+
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
