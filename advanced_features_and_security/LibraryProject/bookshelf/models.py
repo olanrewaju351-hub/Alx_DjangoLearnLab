8b6accd5a5b4e66de563fbd08879a6ac12cd9674
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-11-13 02:53:55 -0800

    feat: add CustomUser model with date_of_birth and profile_photo, update settings and relationships

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,6 +7,19 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+         return self.create_user(username, email, password, **extra_fields)
+
+
+
 
 
 class Book(models.Model):
