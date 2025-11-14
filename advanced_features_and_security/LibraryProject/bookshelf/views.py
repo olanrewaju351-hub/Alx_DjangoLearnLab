1286899c98f9de52d35f8940893bc721276f4ad1
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-11-14 03:56:03 -0800

    Add security settings block (env-based security configuration)

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required, login_required
 from .models import Book
 from .forms import BookForm  # create a simple ModelForm (see below)
+from .forms import ExampleForm
 
 def book_search(request):
     form = BookSearchForm(request.GET or None)
