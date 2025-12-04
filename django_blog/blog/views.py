6b647ae73e697009f6f4b96a4424342d41f03d54
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-12-04 07:44:30 -0800

    fix(blog): add required auth imports and mixins in views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import (
-    ListView, DetailView,
-    CreateView, UpdateView, DeleteView
-)
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Post
 
 
-# Home page shows all posts
 class PostListView(ListView):
     model = Post
     template_name = "blog/home.html"
@@ -17,42 +14,37 @@ class PostListView(ListView):
     ordering = ["-created_at"]
 
 
-# Single post detail page
 class PostDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
-    context_object_name = "post"
 
 
-# Create new post (must be logged in)
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ["title", "content"]
     template_name = "blog/post_form.html"
-    success_url = reverse_lazy("home")
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-# Update post (only author can edit)
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ["title", "content"]
     template_name = "blog/post_form.html"
-    success_url = reverse_lazy("home")
 
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 
-# Delete post (only author)
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "blog/post_confirm_delete.html"
     success_url = reverse_lazy("home")
 
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
